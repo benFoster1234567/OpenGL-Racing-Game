8201a0a3c00847188154312501a48514f75fe92d
benFoster1234567 <bfoster13@ewu.edu>
2026-06-08 14:38:52 -0700

    Updated main

--- OpenGl/src/Main.cpp
+++ OpenGl/src/Main.cpp
@@ -2,13 +2,9 @@
 #ifndef ENABLE_TESTS
 
 #include <GL/glew.h>
-#include "infra/app/Window.h"
 #include <GLFW/glfw3.h>
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include "infra/engine/DebugConsoleUI.h"
 #include "infra/app/Application.h"
+
 int main()
 {
     if (!glfwInit()) { return -1; }
